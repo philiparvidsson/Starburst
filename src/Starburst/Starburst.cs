0fcc2037edf21e23a124d5dfd345bf9b8b5e57a9
tobohr <tobias@ohrstrom.se>
2016-04-19 16:14:58 +0200

    ,

--- src/Starburst/Starburst.cs
+++ src/Starburst/Starburst.cs
@@ -12,7 +12,7 @@ public class Starburst_Game_Impl : Game_Impl {
     public override void init() {
         Game_Engine.inst().add_subsystems(
             new Engine.Subsystems.Position_Integrator(),
-            new Engine.Subsystems.Sprite_Renderer(new SpriteBatch(Game_Engine.inst().GraphicsDevice))
+            new Engine.Subsystems.Sprite_Renderer(new SpriteBatch(Game_Engine.inst().GraphicsDevice)),
             new Engine.Subsystems.Window_Title_Writer()
         );
 
