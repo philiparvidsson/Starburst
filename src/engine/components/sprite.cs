6441f84d7c05c8434f12de61101a5cc0860db8e3
Philip Arvidsson <philip@philiparvidsson.com>
2016-04-21 12:17:39 +0200

    reset....

---
+++ src/engine/components/sprite.cs
@@ -0,0 +1,19 @@
+namespace Fab5.Engine.Components {
+
+/*------------------------------------------------
+ * USINGS
+ *----------------------------------------------*/
+
+using Fab5.Engine.Core;
+
+using Microsoft.Xna.Framework.Graphics;
+
+/*------------------------------------------------
+ * CLASSES
+ *----------------------------------------------*/
+
+public class Sprite : Component {
+    public Texture2D texture;
+}
+
+}
---
+++ src/engine/components/velocity.cs
@@ -0,0 +1,20 @@
+namespace Fab5.Engine.Components {
+
+/*------------------------------------------------
+ * USINGS
+ *----------------------------------------------*/
+
+using Fab5.Engine.Core;
+
+using System.Collections.Generic;
+
+/*------------------------------------------------
+ * CLASSES
+ *----------------------------------------------*/
+
+public class Velocity : Component {
+    public float x;
+    public float y;
+}
+
+}
