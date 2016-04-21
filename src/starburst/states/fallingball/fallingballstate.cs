dd7d3eb1f0937bf296663202448f11a2db774b5a
Philip Arvidsson <philip@philiparvidsson.com>
2016-04-21 12:16:18 +0200

    reset

---
+++ src/starburst/states/fallingball/fallingballstate.cs
@@ -0,0 +1,38 @@
+namespace Fab5.Starburst.States {
+
+using Fab5.Engine.Core;
+using Fab5.Engine.Subsystems;
+
+using Fab5.Starburst.States.Falling_Ball;
+
+using Fab5.Engine;
+using Fab5.Engine.Components;
+using Fab5.Engine.Core;
+using Fab5.Engine.Subsystems;
+
+using Fab5.Starburst.States.Falling_Ball.Entities;
+
+using Microsoft.Xna.Framework.Graphics;
+
+public class Falling_Ball_State : Game_State {
+
+    public override void init() {
+        add_subsystems(
+            new Position_Integrator(),
+            new Sprite_Renderer(new SpriteBatch(Starburst.inst().GraphicsDevice))
+        );
+
+        create_entity(Ball.create_components());
+    }
+
+    public override void update(float t, float dt) {
+        base.update(t, dt);
+
+        if (t > 2.0f) {
+            Starburst.inst().leave_state();
+        }
+    }
+
+}
+
+}
