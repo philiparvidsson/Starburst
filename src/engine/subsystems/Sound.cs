0a7f8372893c244af42f92b457bc04021398756d
tobohr <tobias@ohrstrom.se>
2016-04-25 14:08:32 +0200

    music

--- src/engine/subsystems/Sound.cs
+++ src/engine/subsystems/Sound.cs
@@ -13,28 +13,44 @@ namespace Fab5.Engine.Subsystems
         {
             int num_components;
             var entities = Fab5_Game.inst().get_entities(out num_components,
-                typeof(BackgroundMusic),
+                typeof(BackgroundMusicLibrary),
                 typeof(Fab5SoundEffect));
 
             for (int i = 0; i < num_components; i++)
             {
                 var entity = entities[i];
-                var music = entity.get_component<BackgroundMusic>();
+                var music = entity.get_component<BackgroundMusicLibrary>();
                 if (!music.IsSongStarted)
                 {
-                    MediaPlayer.Play(music.BackSong);
-                    MediaPlayer.IsRepeating = music.IsRepeat;
+                    MediaPlayer.Play(music.Library.FirstOrDefault().BackSong);
+                    MediaPlayer.IsRepeating = music.Library.FirstOrDefault().IsRepeat;
+                    music.NowPlayingIndex = 0;
                     music.IsSongStarted = true;
                 }
+
                 var gameinst = Fab5_Game.inst();
                 var keypressed = gameinst.MessagesQueue.Where(x => x.EventType == "KeyPressed").ToList();
                 foreach(var item in keypressed)
                 {
-                    if(item.EventName == "Fire") {
-                        
+                    if(item.EventName == "Fire"){
                         var effect = entity.get_component<Fab5SoundEffect>();
                         var ins = effect.SoundEffect.CreateInstance();
                         ins.Play();
+                    }
+                    var timesince =  DateTime.Now- music.LastChanged;
+                    if(item.EventName == "ChangeBackSong" && timesince.Seconds > 0.2)
+                    {
+                        MediaPlayer.Stop();
+                        music.NowPlayingIndex++;
+                        if (music.NowPlayingIndex == music.Library.Count)
+                            music.NowPlayingIndex = 0;
+                        var song = music.Library.ElementAt(music.NowPlayingIndex);
+                        Console.WriteLine("song changed to" + song.File);
+
+                        MediaPlayer.Play(song.BackSong);
+                        MediaPlayer.IsRepeating = song.IsRepeat;
+                        music.IsSongStarted = true;
+                        music.LastChanged = DateTime.Now;
                     }
                 }
             }
