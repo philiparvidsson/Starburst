6395339aa387c251d37ddc82b7b751adacf5b1c3
tobiasanhed <Anhedtobias@gmail.com>
2016-04-27 12:02:21 +0200

    Added score component to player_ship. Also added phil's missed shit...

---
+++ src/engine/components/score.cs
@@ -0,0 +1,24 @@
+﻿namespace Fab5.Engine.Components
+{
+
+    /*------------------------------------------------
+     * USINGS
+     *----------------------------------------------*/
+
+    using Fab5.Engine.Core;
+
+
+    /*------------------------------------------------
+     * CLASSES
+     *----------------------------------------------*/
+
+    public class Score : Component
+    {
+        public int score;
+        public Score()
+        {
+            score = 0;
+        }
+    }
+
+}
