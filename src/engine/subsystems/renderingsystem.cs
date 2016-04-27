b2d434e91f525ded71e581a5425e95acb247f48b
Philip Arvidsson <philip@philiparvidsson.com>
2016-04-27 12:07:28 +0200

    map edit fix

--- src/engine/subsystems/renderingsystem.cs
+++ src/engine/subsystems/renderingsystem.cs
@@ -106,8 +106,10 @@ namespace Fab5.Engine.Subsystems {
             for (int i = left; i <= right; i++) {
                 float y = 0.0f;
                 for (int j = top; j <= bottom; j++) {
-                    if (i < 0 || i > 255 || j < 0 || j > 255)
+                    if (i < 0 || i > 255 || j < 0 || j > 255) {
+                        y += th;
                         continue;
+                    }
 
                     int o = i + (j*256);
 
