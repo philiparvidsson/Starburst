aab36872ec92202d9a7e10a6f14452eb2e1d417b
Philip Arvidsson <philip@philiparvidsson.com>
2016-05-03 23:19:50 +0200

    more optimizations ....

---
+++ old_code/textrenderer.cs
@@ -0,0 +1,39 @@
+namespace Fab5.Engine.Subsystems {
+
+using Fab5.Engine.Components;
+using Fab5.Engine.Core;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+public class Text_Renderer : Subsystem {
+    private readonly SpriteBatch sprite_batch;
+
+    public Text_Renderer(SpriteBatch sprite_batch) {
+        this.sprite_batch = sprite_batch;
+    }
+
+    public override void draw(float t, float dt) {
+        var entities = Fab5_Game.inst().get_entities_fast(typeof (Text));
+        int num_components = entities.Count;
+
+        sprite_batch.Begin();
+
+        for (int i = 0; i < num_components; i++) {
+            var entity   = entities[i];
+            var position = entity.get_component<Position>();
+            var text     = entity.get_component<Text>();
+
+            var str = text.format;
+
+            // @To-do: Interpolate string here.
+
+            sprite_batch.DrawString(text.font, str, new Vector2(position.x, position.y), Color.Black, 0.0f, Vector2.Zero, 1.0f, SpriteEffects.None, 0.5f);
+
+        }
+
+        sprite_batch.End();
+    }
+}
+
+}
