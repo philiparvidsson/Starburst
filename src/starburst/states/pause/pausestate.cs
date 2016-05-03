9fb750900947dda21031a2b53c0216f5b1112861
Philip Arvidsson <philip@philiparvidsson.com>
2016-05-03 09:24:20 +0200

    lower volume while paused

--- src/starburst/states/pause/pausestate.cs
+++ src/starburst/states/pause/pausestate.cs
@@ -8,12 +8,14 @@ using Fab5.Engine.Subsystems;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 using System;
 using System.Threading;
 
 public class Pause_State : Game_State {
     private bool can_unpause = false;
+    private float restore_vol;
 
     public override void init() {
         var sprite_batch = new SpriteBatch(Starburst.inst().GraphicsDevice);
@@ -32,18 +34,23 @@ public class Pause_State : Game_State {
         GFX_Util.draw_def_text(sprite_batch, "Paused", tx, ty);
 
         sprite_batch.End();
+
+        restore_vol = MediaPlayer.Volume;
+        MediaPlayer.Volume *= 0.4f;
     }
 
     public override void draw(float t, float dt) {
         if (can_unpause) {
             for (int i = 0; i <= 3; i++) {
                 if (GamePad.GetState((PlayerIndex)i).IsConnected && GamePad.GetState((PlayerIndex)i).Buttons.Start == ButtonState.Pressed) {
+                    MediaPlayer.Volume = restore_vol;
                     Starburst.inst().leave_state();
                     return;
                 }
             }
 
             if (Keyboard.GetState().IsKeyDown(Keys.P)) {
+                MediaPlayer.Volume = restore_vol;
                 Starburst.inst().leave_state();
                 return;
             }
