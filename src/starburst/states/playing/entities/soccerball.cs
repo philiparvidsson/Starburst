13c3b913bc146174a489cdcb2c509c4cd723c020
Philip Arvidsson <philip@philiparvidsson.com>
2016-05-16 14:10:44 +0200

    grass tiles

--- src/starburst/states/playing/entities/soccerball.cs
+++ src/starburst/states/playing/entities/soccerball.cs
@@ -120,7 +120,7 @@ public static class Soccer_Ball {
                                         return;
                                     }*/
 
-                                    var text = string.Format("{0} TEAM\nSCORED A GOAL!!", scoring_team == 1 ? "RED" : "BLUE");
+                                    var text = string.Format("{0} TEAM SCORED A GOAL!!", scoring_team == 1 ? "RED" : "BLUE");
                                     var ts   = GFX_Util.measure_string(text);
 
                                     // @To-do: larger text plz
